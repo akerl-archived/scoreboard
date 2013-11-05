76b9fbc1c62073d0d54cdfd1ee02db54963b11e5
Les Aker <me@lesaker.org>
2013-11-05 15:12:50 -0500

    fancier method for less lag

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'faraday-http-cache'
 require 'yaml'
 require 'github_stats'
 require 'basic_cache'
+require 'json'
 
 Config = YAML.load open('config.yaml').read
 Client = Octokit::Client.new :login => Config['username'], :password => Config['password']
@@ -15,22 +16,49 @@ API_Cache = Faraday::Builder.new do |builder|
 end
 Octokit.middleware = API_Cache
 
-Player = Struct.new(:name, :is_self?, :stats)
 Stats_Cache = Basic_Cache::Time_Cache.new(900)
 
-get %r{^/([a-zA-Z-]+)?$} do |name|
-    name ||= Config['username']
-    players = [
-        Player.new(name, true),
-        *Client.following(name).map { |x| Player.new(x.login, false) }
-    ].map do |player|
-        player.stats = Stats_Cache.cache(player.name) { Github_Stats.new(player.name) }
-        player
-    end.sort { |a, b| b.stats.streak.length <=> a.stats.streak.length }
-    players.inject('') do |acc, x|
-        acc << "#{x.name} has a streak of #{x.stats.streak.length}"
-        acc << ", but has not committed today" if x.stats.today.zero?
-        acc << "<br />\n"
+class Player
+    attr_reader :name, :stats
+
+    def initialize(name)
+        @name = name
+        @stats = Stats_Cache.cache(name) { Github_Stats.new(name) }
     end
+
+    def export
+        {
+            :score => @stats.streak.length
+        }
+    end
+end
+
+get '/:name/stats' do |name|
+    headers 'Content-Type' => 'application/json'
+    begin
+        data = Player.new(name).export.to_json
+    rescue
+        halt 500, '{}'
+    end
+    body data
+end
+
+get '/:name/following' do |name|
+    headers 'Content-Type' => 'application/json'
+    begin
+        data = Client.following(name).map { |x| x.login }.to_json
+    rescue
+        halt 500, '{}'
+    end
+    body data
+end
+
+get '/:name' do |name|
+    @player_one = name
+    erb :scoreboard
+end
+
+get '/' do
+    redirect to("/#{Config['username']}")
 end
 
